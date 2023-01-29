63c39522c7099a0a4729a5757dc5eca7d2006c20
Pushkar <pushkar14.patil@gmail.com>
2023-01-29 23:57:12 +0530

    added aos and made some supplimentary changes

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,57 +1,75 @@
 import { Container } from "react-bootstrap";
 import hack2skill from "../assets/img/h2skill.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
+import "animate.css";
 import TitleSponsorsCard from "./TitleSponsorsCard";
 import MediumSponsorsCard from "./MediumSponsorsCard";
 import GeneralSponsorsCard from "./GeneralSponsorsCard";
 
 export const Sponsors = () => {
-
-    return (
-        <section className="project" id="project">
-            <Container>
-                <div className="relative p-4 lg:py-10 w-full">
-                <div class="absolute h-[2px] bg-[#7b1072] w-56"></div>
-                    <h1 className="text-[2.5rem] tracking-[.05em] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] capitalize">
-                    OUR SPONSORS
-                    </h1>
-                </div>
-                <div className="p-6 lg:px-56 lg:pt-10 space-y-8 fonts-space-grotesk text-[#fafafa] flex space-x-2 justify-center">
-                    <h1 className="capitalize text-[2rem] lg:text-[2.5rem] font-normal">
-                        TITLE SPONSOR
-                    </h1>
-                </div>
-                <TitleSponsorsCard image={hack2skill} dataAos="fade-up" />
-                <div className="p-4 fonts-space-grotesk">
-                    <hr className="text-[#767676] dark:text-[#767676]" />
-                </div>
-                <div className="grid grid-cols-1 gap-2 md:grid md:grid-cols-2 md:gap-3">
-                    <MediumSponsorsCard title="EVENT PARTNER" image={hack2skill} dataAos="fade-right" />
-                    <MediumSponsorsCard title="ASSOCIATE SPONSOR" image={hack2skill} dataAos="fade-left" />
-                    <MediumSponsorsCard title="UPSKILLING PARTNER" image={hack2skill} dataAos="fade-right" />
-                    <MediumSponsorsCard title="SILVER SPONSORS" image={hack2skill} dataAos="fade-left" />
-                </div>
-                <div className="pt-10">
-                    <hr className="text-[#767676] dark:text-[#767676]" />
-                </div>
-                <div className="p-6 lg:px-56 lg:pt-10 space-y-8 fonts-space-grotesk text-[#fafafa] flex space-x-2 justify-center">
-                    <h1 className="capitalize text-[1.4rem] lg:text-[1.6rem] font-normal">
-                        GENERAL SPONSOR
-                    </h1>
-                </div>
-                <div className="grid grid-cols-1 gap-4 xl:grid xl:grid-cols-4 xl:gap-5 md:grid md:grid-cols-2 md:gap-3">
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                    <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
-                </div>
-            </Container>
-            <img className="background-image-right" src={colorSharp2}></img>
-        </section>
-    )
-}
+  return (
+    <section className="project" id="project">
+      <Container>
+        <div className="relative p-4 lg:py-10 w-full">
+          <h1
+            className="text-[45px] sm:text-[25px] md:text-[35px] lg:text-[45px] capitalize"
+            style={{ fontFamily: "Helvetica Bold" }}
+          >
+            Our Sponsors
+          </h1>
+          <div class="absolute h-[2px] bg-[#7b1072] w-56" />
+        </div>
+        <div className="p-6 lg:px-56 lg:pt-10 space-y-8 fonts-space-grotesk text-[#fafafa] flex space-x-2 justify-center">
+          <h1 className="capitalize text-[2rem] lg:text-[2.5rem] font-normal">
+            Title Sponsor
+          </h1>
+        </div>
+        <TitleSponsorsCard image={hack2skill} dataAos="fade-up" />
+        <div className="p-4 fonts-space-grotesk">
+          <hr className="text-[#767676] dark:text-[#767676]" />
+        </div>
+        <div className="grid grid-cols-1 gap-2 md:grid md:grid-cols-2 md:gap-3">
+          <MediumSponsorsCard
+            title="Event Sponsors"
+            image={hack2skill}
+            dataAos="fade-right"
+          />
+          <MediumSponsorsCard
+            title="Associate Sponsors"
+            image={hack2skill}
+            dataAos="fade-left"
+          />
+          <MediumSponsorsCard
+            title="Upskilling Sponsors"
+            image={hack2skill}
+            dataAos="fade-right"
+          />
+          <MediumSponsorsCard
+            title="Silver Sponsors"
+            image={hack2skill}
+            dataAos="fade-left"
+          />
+        </div>
+        <div className="pt-10">
+          <hr className="text-[#767676] dark:text-[#767676]" />
+        </div>
+        <div className="p-6 lg:px-56 lg:pt-10 space-y-8 fonts-space-grotesk text-[#fafafa] flex space-x-2 justify-center">
+          <h1 className="capitalize text-[1.4rem] lg:text-[1.6rem] font-normal">
+            General Sponsors
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 gap-4 xl:grid xl:grid-cols-4 xl:gap-5 md:grid md:grid-cols-2 md:gap-3">
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+          <GeneralSponsorsCard image={hack2skill} dataAos="fade-up" />
+        </div>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
