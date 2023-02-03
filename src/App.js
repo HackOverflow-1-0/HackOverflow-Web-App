af98e7fb288aa371f3866487f7cff3fc6f60f4ca
Pushkar <pushkar14.patil@gmail.com>
2023-02-03 16:14:05 +0530

    added bottom navbar for mobile view

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ import { Contact } from "./components/Contact";
 import Footer from "./components/Footer";
 import { Schedule } from "./components/Schedule";
 import HackathonOverview from "./components/HackthonOverview";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Themes from "./components/Themes";
 import { Sponsors } from "./components/Sponsors";
 import FAQ from "./components/FAQ";
 import Prizes from "./components/Prizes/Prizes";
 import About from "./components/About";
+import MobileNavbar from "./components/MobileNavbar/MobileNavbar";
 
 function App() {
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
   return (
     <Fragment>
       <NavBar />
@@ -27,6 +42,7 @@ function App() {
       <Contact />
       <About />
       <Footer />
+      {windowSize[0] < 600 && <MobileNavbar />}
     </Fragment>
   );
 }
