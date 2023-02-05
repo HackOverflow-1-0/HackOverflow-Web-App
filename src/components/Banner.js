e27cf16de1359d155f2fdd97b520b2b9f5604ea5
Shreeyash Gurav <ssgurav28@gmail.com>
2023-02-05 20:32:54 +0530

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,94 +1,129 @@
-import { React, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/banner-illustration.png";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import "animate.css";
-import Typewriter from "typewriter-effect";
-
 import TrackVisibility from "react-on-screen";
 import classes from "./Banner.module.css";
 import Countdown from "react-countdown";
-import CountDownTimer from "./CountDown/CountDownTimer";
-import SocialMedia from "./SocialMedia/SocialMedia";
+
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import "./Banner.css";
+
 export const Banner = () => {
-  const Slogan = () => {
-    return (
-      // eslint-disable-next-line
-      <span className={"slogan sloganBeginning"} role="text">
-        <h1 className={"sloganBeginning"}>
-          HackOverflow 1.0
-          <h2 className="animatedtext text-[#8D1F97] text-[2rem]">
-            &nbsp;
-            <span className={"typewriterPlaceholder"}>
-              <TypewriterWelcome />
-            </span>
-          </h2>
-        </h1>
-      </span>
-    );
-  };
+  const [width, setWindowWidth] = useState(0);
+  useEffect(() => {
+    updateDimensions();
 
-  const TypewriterWelcome = () => {
-    const [displayPlaceholder, setDisplayPlaceholder] = useState(true);
-    const strings = [
-      "Save the date 16th March 2023.",
-      "Compete with the best.",
-      "Register today on Devfolio.",
-      "Explore Different Themes.",
-    ];
-    const typewriterInit = (typewriter) => {
-      if (displayPlaceholder) {
-        setDisplayPlaceholder(false);
-      }
-      const typeString = (string, pauseTime) => {
-        typewriter.typeString(string).pauseFor(pauseTime).deleteAll(30);
-      };
-      for (const string of strings) {
-        typeString(string, 2500);
-      }
-      typewriter.start();
-    };
-    return (
-      <>
-        {displayPlaceholder && (
-          <span className={"typewriterPlaceholder"}>inspiration.</span>
-        )}
-        <Typewriter
-          options={{
-            autoStart: true,
-            loop: true,
-            delay: 30,
-            deleteSpeed: 30,
-          }}
-          onInit={(typewriter) => typewriterInit(typewriter)}
-        />
-      </>
-    );
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
   };
-
+  const [tickerVisible, setTickerVisible] = useState(false);
+  const date = new Date("2023-03-17");
+  const toggleText = tickerVisible ? "Hide Countdown" : "Show Countdown";
   return (
-    <>
-      <section className="banner" id="home">
-        <div className="px-28">
-          <Col xs={12} md={6} xl={7}>
-            <Slogan />
-          </Col>
-        </div>
-
-        <SocialMedia />
-        <div className="px-28">
-          <Col xs={12} md={6} xl={7}>
-            <CountDownTimer />
-            <p className="banner-content" style={{ fontSize: "1.5vw" }}>
-              Join us on 17th March 2023 with students from across the nation
-              for 36 hours of creation, innovation, & fun.
-            </p>
-            <div className={classes.registerButton}>
-              <button>Register</button>
+    <section className="section banner" id="home">
+      <div className="container">
+        <div className="bannerWrap">
+          <div className="welcometoText">Welcome to</div>
+          <div className="middleTitle">
+            <svg viewBox="0 0 1320 230">
+              <text x="50%" y="50%" dy=".35em" textAnchor="middle">
+                HackOverflow 1.0
+              </text>
+            </svg>
+            <div className="orgText">
+              Organized by <span>Computer department of PHCET</span>
             </div>
-          </Col>
+          </div>
+          <div className="slogan">Think. Code. Innovate.</div>
+          <div className="countDownContainer">
+            <FlipClockCountdown
+              to={date}
+              labelStyle={{
+                fontFamily: "Centra",
+                textTransform: "uppercase",
+                paddingTop: "5px",
+                fontWeight: 500,
+                fontSize: width > 400 ? "24px" : "12px",
+              }}
+              digitBlockStyle={{
+                width: width > 400 ? 50 : 30,
+                height: width > 400 ? 60 : 40,
+                fontSize: width > 400 ? "40px" : "25px",
+                fontFamily: "Aldrich",
+                backgroundColor: "purple",
+              }}
+            />
+          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
+
+// {/* <Row className="aligh-items-center">
+//   <Col xs={12} md={6} xl={7}>
+//     <TrackVisibility>
+//       {({ isVisible }) => (
+//         <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+//           {/* <span className="tagline">Welcome to my Portfolio</span> */}
+//           <h1 id="main-text" style={{ height: "160px" }}>
+//             {`HackOverflow 1.0`}
+//             <br />
+//             <span
+//               className="txt-rotate"
+//               dataPeriod="1000"
+//               data-rotate='[ "16th March 2023", "Compete with the best", "Register today on Devfolio" ]'
+//             >
+//               <span className="wrap">{text}</span>
+//             </span>
+//           </h1>
+//           {/* <Countdown
+//                     date={date + 10000}
+//                     renderer={({ days, hours, minutes, seconds }) => {
+//                       return (
+//                         <>
+//                           <span className="tagline">Starts In</span>
+//                           <h1 id={classes.countDown}>
+//                             <span className={classes.countDown}>{days}d</span>:
+//                             <span className={classes.countDown}>{hours}h</span>:
+//                             <span className={classes.countDown}>
+//                               {minutes}m
+//                             </span>
+//                             :
+//                             <span className={classes.countDown}>
+//                               {seconds}s
+//                             </span>
+//                           </h1>
+//                         </>
+//                       );
+//                     }}
+//                   /> */}
+//           <div className="">
+//             <CountDownTimer />
+//           </div>
+//           <p className="banner-content" style={{ fontSize: "1.5vw" }}>
+//             Join us on 17th March 2023 with students from across the nation for
+//             36 hours of creation, innovation, & fun.
+//           </p>
+//           <button onClick={() => console.log("connect")}>Register</button>
+//         </div>
+//       )}
+//     </TrackVisibility>
+//   </Col>
+//   <Col xs={12} md={6} xl={5}>
+//     <TrackVisibility>
+//       {({ isVisible }) => (
+//         <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+//           <img src={headerImg} alt="Header Img" />
+//         </div>
+//       )}
+//     </TrackVisibility>
+//   </Col>
+// </Row>; */}
