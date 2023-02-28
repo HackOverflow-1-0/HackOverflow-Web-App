1058efb13f8a24b00a03526758fbde16da6aa631
rajatrai30 <mailmerairajat@gmail.com>
2023-02-28 21:12:00 +0530

    Instagram Api Integration Update

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -16,7 +16,7 @@ const PreLoader = () => {
     // <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
     <div>
       <video
-        className="relative bg-video video"
+        className="preloadervideo"
         autoPlay
         loop
         muted
