337ae0c6767298731d7486da6180fde70f01402e
rajatrai30 <mailmerairajat@gmail.com>
2023-03-10 00:28:10 +0530

    App js Update

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 6000);
     return () => clearTimeout(timer);
   }, []);
 
