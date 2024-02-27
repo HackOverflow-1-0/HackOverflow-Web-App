a98254a06e1317a718307d88db2e46d77ba16630
rajatrai30 <mailmerairajat@mail.com>
2024-02-27 22:51:34 +0530

    Preloader Context update

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -1,24 +1,52 @@
 import React from "react";
 import "./PreLoader.css";
 import Lottie from "lottie-react";
+import { Typewriter } from "react-simple-typewriter";
 import animationData from "../../assets/Lotties/preloader2.json";
 
 const PreLoader = () => {
   return (
     <div className="flex flex-row items-center justify-center preloader">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2">
         <Lottie className="w-[50rem] h-72" animationData={animationData} />
-        {/* <p
-          id="orange-pink"
-          className="text-[30px] sm:text-[36px] md:text-[35px] lg:text-[45px]"
+        <p
+          // id="orange-pink"
+          className="flex text-[20px] sm:text-[25px] md:text-[28px] lg:text-[35px]"
           style={{
             fontFamily: "Poppins,sans-serif",
-            fontWeight: 700,
+            fontWeight: 500,
             textAlign: "center",
           }}
         >
-          Loading...
-        </p> */}
+          {/* Loading HackOverflow Universe...{" "} */}
+          {/* <span id="green-yellow"> */}
+          <span className="w-[24rem] sm:w-fit">
+            <Typewriter
+              // words={["Initialize💡", "Compile🛠️", "Debug🔍", "Deploy🚀"]}
+              words={[
+                "Loading the innovation hub.🌟",
+                "Crafting HackOverflow tech.🌐🌌",
+                "Preparing for code revolution.💻",
+                "Building the future at HackOverflow 2.0.🚀",
+                "Assembling the coding cosmos.🌌",
+                "Fueling the coding galaxy.🌟",
+                "Empowering developers worldwide.💪",
+                "Shaping the digital frontier.🌐",
+                "Harnessing the power of code.⚡",
+                "Unlocking tech potiential @HackOverflow.🔓",
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="|"
+              cursorBlinking="true"
+              cursorColor="inherit"
+              typeSpeed={50}
+              deleteSpeed={50}
+              delaySpeed={1000}
+              // onType={handleType}
+            />
+          </span>
+        </p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+Kommunicate.init(process.env.REACT_APP_CHAT_TOKEN, {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
